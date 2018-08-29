7a5e4db06c8e1a595571623f0997987e486d8ebc
Kabanga Bill <tkbillkabanga@gmail.com>
2018-08-29 23:08:30 +0300

    Modifies user model[#160040058]

--- api/models/usermodel.py
+++ api/models/usermodel.py
@@ -11,32 +11,23 @@ class Users(object):
         self.email = email
         self.password = password
 
-    def input_validate_user(self):
-        if not re.match(r"([\w\.-]+)@([\w\.-]+)(\.[\w\.-]+$)",self.email):
-            return {'message':'wrong email entry'}
-        query = "SELECT * FROM users WHERE email= '{}'".format(self.email)
-        exist = db.get_user(query)
-        if exist:
-            return {'message':'User already exists'}
-
-
-        
+    # def input_validate_user(self):
+    #     if not re.match(r"([\w\.-]+)@([\w\.-]+)(\.[\w\.-]+$)",self.email):
+    #         return {'message':'wrong email entry'}
+    #     query = "SELECT * FROM users WHERE email= '{}'".format(self.email)
+    #     exist = db.get_user(query)
+    #     if exist:
+    #         return {'message':'User already exists'}
+   
     def save(self):
-        query = "SELECT * FROM users WHERE email= '{}'".format(self.email)
-        exist = db.get_user(query)
-        if exist:
-            return {'message':'User already exists'}
-        message = self.input_validate_user()
-        if not message:
-            query= "INSERT INTO users(username,email,password) VALUES('{}','{}','{}') RETURNING id".format(self.username,
+            query= "INSERT INTO users(username,email,password) VALUES('{}','{}','{}') RETURNING usrid".format(self.username,
             self.email,self.password)
             return db.post_data(query)
-        return message
         
 
     def get_specific_user(self,username):
         query = "SELECT * FROM users WHERE username = '{}'".format(self.username)
-        return db.get_user(query)
+        return db.get_data(query)
         
     
 
