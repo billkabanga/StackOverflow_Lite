5dcd9d3a56feca64b21a7da186f77c608f3b283f
Kabanga Bill <tkbillkabanga@gmail.com>
2018-08-29 09:57:59 +0300

    Modifies database controller file

--- dbcontroller.py
+++ dbcontroller.py
@@ -14,14 +14,15 @@ class Dbcontroller(object):
         except:
             print ('Database not available')
 
-    def get_user(self,query):
-        self.cursor.execute(query)
-        return self.cursor.fetchone()
+   
 
     def create_tables(self):
-        user_table = "CREATE TABLE IF NOT EXISTS users(id serial PRIMARY KEY, username varchar(50), email varchar(100), password varchar(20)"
-        questions_table = "CREATE TABLE IF NOT EXISTS questions(id serial PRIMARY KEY, question(100), FOREIGN KEY(user) REFERENCES user_table(id)"
-        answer_table = "CREATE TABLE IF NOT EXISTS answers(id serial PRIMARY KEY, question(100), FOREIGN KEY(question) REFERENCES questions_table(id), FOREIGN KEY(user) REFERENCES user_table(id)"
+        user_table = "CREATE TABLE IF NOT EXISTS users(id serial PRIMARY KEY, username varchar(50),\
+         email varchar(100), password varchar(20))"
+        questions_table = "CREATE TABLE IF NOT EXISTS questions(id serial PRIMARY KEY, question varchar(100),\
+         FOREIGN KEY(id) REFERENCES users(id))"
+        answer_table = "CREATE TABLE IF NOT EXISTS answers(id serial PRIMARY KEY, answer varchar(100),\
+         FOREIGN KEY(id) REFERENCES questions(id), FOREIGN KEY(id) REFERENCES users(id))"
 
         self.cursor.execute(user_table)
         self.cursor.execute(questions_table)
@@ -29,19 +30,21 @@ class Dbcontroller(object):
 
     def post_data(self,query):
         self.cursor.execute(query)
-        re = self.cursor.fetchone()
 
-        return re
+    def get_user(self,query):
+        self.cursor.execute(query)
+        user = self.cursor.fetchone()
+        return user
     
     def get_all_questions(self):
         self.cursor.execute("SELECT * FROM questions")
         questions = self.cursor.fetchall()
-        
         return questions
 
     def get_all_answers(self):
         self.cursor.execute("SELECT * FROM answers")
         answers = self.cursor.fetchall()
+        return answers
 
     def get_specific_question(self,id):
         self.cursor.execute("SELECT * FROM questions id = '{}' ".format(id))
@@ -56,8 +59,7 @@ class Dbcontroller(object):
         update_command = "UPDATE users SET   WHERE "
         self.cursor.execute(update_command)
 
-if __name__ == '__main__':
-    database_connection = Dbcontroller()
+    
 
 
             
